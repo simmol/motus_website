420f78ee838c9e2ed7c295f64fd501ad6ebdf3a7
Pavlin Angelov <fonix.simmol@gmail.com>
2012-07-05 01:54:26 +0300

    implement new multilang schema implement slugs build single post view ;

---
+++ newproject/translation.py
@@ -0,0 +1,7 @@
+from modeltranslation.translator import translator, TranslationOptions
+from blog.models import Post
+
+class PostTranslationOptions(TranslationOptions):
+  fields = ('title', 'body')
+
+translator.register(Post, PostTranslationOptions)
