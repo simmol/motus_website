bd669049f06e0ba997acefe5a2e02a4636084957
Pavlin Angelov <fonix.simmol@gmail.com>
2012-09-13 22:22:25 +0300

    fix pages urls to not colide with gallery and others

--- pages/urls.py
+++ pages/urls.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView, DetailView, ListView
 from django.conf import settings
 
 urlpatterns = patterns('pages.views',
-  url('page/(?P<page_id>\d+)', 'page', name='page-details'),
-  url('page/(?P<slug>[-\w]+)', 'page', name='page-details-slug'),
+  url('^page/(?P<page_id>\d+)', 'page', name='page-details'),
+  url('^page/(?P<slug>[-\w]+)', 'page', name='page-details-slug'),
 
-  url('library/article/(?P<slug>[-\w]+)', 'article', name='article'),
+  url('^library/article/(?P<slug>[-\w]+)', 'article', name='article'),
 )
