3e0dc97948404e4aa1e1cd5f21e35dee153006c8
Pavlin Angelov <fonix.simmol@gmail.com>
2012-09-16 19:53:01 +0300

    side bar lattest photos slider

--- photologue/templatetags/photologue_sidebar.py
+++ photologue/templatetags/photologue_sidebar.py
@@ -6,6 +6,6 @@ from photologue.models import Photo
 
 @register.inclusion_tag('photologue/tags/lattest_photos.html', takes_context=True)
 def lattest_photos(context):
-  lattest_photos = Photo.objects.all().filter(is_public= True).order_by('date_added')[:6]
+  lattest_photos = Photo.objects.all().filter(is_public=True).order_by('date_added')[:6]
   context['lattest_photos'] = lattest_photos
   return context
