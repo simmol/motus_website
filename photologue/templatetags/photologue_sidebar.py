35af92d30cb0b3f2127999c19f1430dd146ac119
Pavlin Angelov <fonix.simmol@gmail.com>
2012-09-16 14:18:31 +0300

    add photologue in the project - better easy extend then updating

---
+++ photologue/templatetags/photologue_sidebar.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+from photologue.models import Photo
+
+@register.inclusion_tag('photologue/tags/lattest_photos.html', takes_context=True)
+def lattest_photos(context):
+  lattest_photos = Photo.objects.all().filter(is_public= True).order_by('date_added')[:6]
+  context['lattest_photos'] = lattest_photos
+  return context
