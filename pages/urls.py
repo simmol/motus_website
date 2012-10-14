68dbf4fa64b04aed33bbc71b2af76302044b01c0
Pavlin Angelov <fonix.simmol@gmail.com>
2012-10-15 00:58:15 +0300

    create general listing for all pages of given type - pages, library, armory etc

--- pages/urls.py
+++ pages/urls.py
@@ -9,6 +9,11 @@ urlpatterns = patterns('pages.views',
   url('^blog/article/(?P<slug>[-\w]+)', 'blog_article', name='blog-article'),
   url('^events/event/(?P<slug>[-\w]+)', 'event', name='event'),
 
+  url('^pages$', 'pages_articles', {'content_type':'SYS'}, name="pages-articles"),
+  url('^library$', 'pages_articles', {'content_type':'LIB'}, name='library0articles'),
+  url('^armory', 'pages_articles', {'content_type':'ARM'}, name='armory-articles'),
+  url('^events', 'pages_articles', {'content_type':'EVE'}, name='events-articles'),
+  url('^blog', 'pages_articles', {'content_type':'BLG'}, name='blog-articles'),
 
   url('^page/category/(?P<category>[-\w]+)', 'page_category', name='page-category'),
   url('^library/category/(?P<category>[-\w]+)', 'library_category', name='library-category'),
